b1a4e3b435541063f3827bb95118977435b32c87
José Toro <joseluis.toro.pinto@gmail.com>
2020-06-01 22:50:08 -0400

    turns out the assignment does not use backpropagation. dran

--- train_main.py
+++ train_main.py
@@ -1,73 +1,40 @@
 import numpy as np
-
-
-#TODO
-#[ ]Create Layer class (maybe abstraction for input, dense and output)
-##[ ]Set activation function (callback?)
-##[ ]Set topology (input and nodes)
-##[ ]Set foward pass
-##[ ]Set Backpropagation (or whatever the teacher decided to use as learning)
-#[ ]OPTIONAL: Create model function sequential-esque
-#all the after things later... maybe use the model for the whole learning process?
+import pandas as pd
 
 
 
-class Layer:
-    def foward_pass(self,):
-        pass
-    def learn(self, callback):
-        pass
+def softmax(x):
+    pass
 
-class Input(Layer):
-    def __init__(self, input_values):
-       self.nodes = input_values
-       self.samples = input_values.shape[0]
-       self.input_nodes = input_values[0].size
-    def foward_pass(self, ):
-        return self.nodes
+def encode(x, w):
+    pass
 
-class Dense(Layer):
-    def __init__(self, size, weights=None, activation='linear'):
-        self.size = size
-        if weights is None:
-            self.w = np.random.random(size)*2 -1
-        else:
-            self.w = weights
-        self.activation = activation
-    def foward_pass(self, other):
-        output = np.dot(other, self.w)
-        if self.activation == 'linear':
-            return output
-        elif self.activation == 'tanh':
-            return np.tanh(output)
-        elif self.activation == 'sigmoid':
-            return 1/np.exp(output)
-        
-class Output(Layer):
-    "maybe i could use the dense layer"
-    def __init__(self, size):
-        self.w = np.random.random(size)*2 - 1
-        self.size = size
-    def foward_pass(self,other):
-        output = np.dot(other, self.w)
-        return output
+def decode(x):
+    pass
 
-
+def output_learning(H, C, target_output):
+    U = H.T.dot(H)
+    U = U + np.identity(U.shape[0])/C
+    U = np.linalg.pinv(U)
+    U = U.dot(H.T)
+    U = U.dot(target_output)
+    return U
 
 
 if __name__ == '__main__':
-    #maybe test an autoencoder i dont know
-    import pandas as pd
-    df = pd.read_csv('data_input.csv', header = None).values
-        print(df.shape)
-    l1 = Input(df)
-    nodes = 100
-    in_size = l1.input_nodes
-    l2 = Dense((in_size, nodes))
-    nodes = 4
-    #ol = Output()
-    in_syn = l1.foward_pass()
-    syn0 = l2.foward_pass(in_syn)
-    #syn1 =
-    print(syn0.size)
+    df_output = pd.read_csv("data_label.csv", header=None)
+    df_input = pd.read_csv("data_input.csv", header=None)
+    nodes = 50
+    C = 10**6
+    samples = df_input.shape[1]
+    w = np.random.random((samples, nodes))*2 - 1
+    w2 = np.random.random((nodes, samples))*2 -1
+    X = df_input.copy()
+    syn0 = X.dot(w)
+    syn1 = syn0.dot(w2)
+    new = output_learning(syn0, C, X)
+    print(new.shape)
+    
+    print(df_output)
+    
 
